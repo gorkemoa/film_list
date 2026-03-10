da5b5ebde5561ec43600642a5d1ec28840e0d06b
gorkemoa <gorkemoa35@gmail.com>
2026-03-10 16:30:18 +0300

    feat: Update AndroidManifest and Info.plist for screen support and input events; configure main.dart for portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
@@ -21,6 +22,11 @@ import 'core/utils/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   await LocalDb.init();
   await Translations.init();
